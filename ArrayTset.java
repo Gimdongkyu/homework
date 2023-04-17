00e831fdc027da03f41de6485ba115b3aca2e51b
김동규 <107844050+Gimdongkyu@users.noreply.github.com>
2023-04-17 16:44:52 +0900

    Add files via upload
    homework2

---
+++ ArrayTset.java
@@ -0,0 +1,360 @@
+package day11;
+
+import java.util.Scanner;
+
+public class ArrayTset {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		int[] num1 = new int[10];
+//		
+//		
+//		for(int i = 0; i<num1.length;i++) {
+//				num1[i] = sc.nextInt();
+//				System.out.printf("num1[%d] = %d \n",i,num1[i]);
+//		}
+		
+//		char[] ch = new char[3]; // ch[0], ch[1], ch[2] ch.. 참조변수.
+//
+//		ch[0] = 'A';
+//		ch[1] = 'B';
+//		ch[2] = 'C';
+//		
+//		char data = 'A';
+//		
+//		for(int i = 0; i <3; i++) {
+//			
+//			ch[i] = data++;
+//			System.out.println(ch[i]);
+//		}
+//				
+
+//		int[] mul = new int[10];
+//		int num = 1;
+//		for(int i = 0; i < mul.length; i++) {
+//			mul[i] = num;
+//			num *= 10;
+//			System.out.println(mul[i]);
+//		}
+
+			
+		
+		
+		
+		
+//		int num2[] = new int[10];
+//		int max = num2[0];
+//		
+//		for(int i = 0; i<num2.length; i++) {
+//			num2[i] = sc.nextInt();
+//			if(num2[i] > max) {
+//				max = num2[i];
+//			}
+//		}
+//		
+//		System.out.println(max);
+		
+		
+		// 문제 1.
+//		int sum[] = new int[5];
+//		
+//		int num1 = 0;
+//		
+//		for(int i = 0; i<sum.length; i++) {
+//			sum[i] = num1;
+//			num1 += 10;
+//			System.out.printf("num1[%d] = %d \n", i, sum[i]);
+//		}
+		
+		//문제 2. 
+//		float[] arrFloat = new float[3];
+//		double[] arrDouble = new double[3];
+//		boolean[] arrBoolean = new boolean[3];
+//		String[] arrStirng = new String[3];
+		
+		//문제 3.
+//		byte[] arrb = new byte[5];
+//		for(int i = 0; i<arrb.length; i++) {
+//			arrb[i] = (byte)(i+1);
+//		}
+//
+//		char[] arrC = new char[5];
+//		char alpabet = 'A';
+//		for(int i = 0; i<arrC.length; i++) {
+//			arrC[i] = alpabet++;
+//		}
+//		
+//
+//		boolean[] arrB = new boolean[5];
+//		for(int i = 0; i<arrB.length; i++) {
+//			if((i+1) % 2 == 0) {
+//				arrB[i] = true;
+//			}else {
+//				arrB[i] = false;
+//			}
+//		}
+//		
+//		short[] arrS = new short[5];
+//		short bag = 100;
+//		for(int i = 0; i<arrS.length; i++) {
+//			arrS[i] = bag;
+//			bag += 100;
+//		}
+//	
+//		long[] arrL = new long[5];
+//		long sasipo = 90;
+//		
+//		for(int i = 0; i<arrL.length; i++) {
+//			arrL[i] = sasipo;
+//			sasipo += 45;
+//		}
+//		
+//		
+//		float[] arrF = new float[5];
+//		float pie = 3.14f;
+//		
+//		for(int i = 0; i<arrF.length; i++) {
+//			arrF[i] = pie;
+//			pie += 3.14;
+//		}
+
+//		double[] arrD = new double[5];
+//		double pie2 = 3.141592;
+//		for(int i = 0; i<arrD.length; i++) {
+//			arrD[i] = pie2;
+//		}
+		
+		//문제 4.
+//		int[] arrsum = new int[100];
+//		int sum = 0;
+//		
+//		for(int i = 0; i<arrsum.length; i++) {
+//			arrsum[i] = (i+1);
+//			sum += arrsum[i];
+//		}
+//		System.out.println(sum);
+		
+//		//문제 5.
+//		int[] a = {2,10,8,5,3,9};
+//		int sum = 0;
+//		for(int i = 0; i<a.length; i++) {
+//			sum += a[i];
+//		}
+//		System.out.println(sum);
+		
+		//문제 6.
+//		char[] ch = {'a','p','p','l','e'};
+//		for(int i = 0; i<ch.length;i++) {
+//			System.out.println(ch[i]);
+//		}
+//		
+//		for(int i = 4; i >= 0; i--) {
+//			System.out.println(ch[i]);
+//		}
+		
+		//문제7, 8, 9, 10
+//		int[] arrsum = new int[10];
+//		int num = 1;
+//		int sum = 0;
+//		int max = arrsum[0];
+//		
+//		for(int i = 0; i < arrsum.length; i++) {
+//			arrsum[i] = num;
+//			num *= 10;
+//			sum += arrsum[i];
+//		}
+//
+//		System.out.println(sum);
+//		
+//		for(int i = 0; i < 2; i++) {
+//			if(arrsum[i] > max) {
+//				max = arrsum[i];
+//			}
+//		}
+//		System.out.println("index 0,1중 더 큰 수 = " + max);
+//		
+//		for(int i = 0; i < 3; i++) {
+//			if(arrsum[i] > max) {
+//				max = arrsum[i];
+//			}
+//		}
+//
+//		System.out.println("index 0,1,2중 더 큰 수 = " + max);
+		
+		
+		//문제 11.
+//		int[] a = new int[10];
+//		int key = 8;
+//		for(int i = 0; i<10; i++) {
+//			a[i] = i;
+//		}
+//		
+//		for(int i = 0; i<10; i++) {
+//			System.out.println(a[i]);
+//			if(key == a[i]) {
+//				System.out.println("키를 찾았습니다.");
+//				break;
+//			}
+//		}
+		
+		//문제 12.
+//		for(int i = 0; i < 10; i++) {
+//			System.out.println(i);
+//		}
+	
+		//문제 13.
+//		int[] a = new int[6];
+//		int sum = 0;
+//		
+//		for(int i = 0; i < a.length; i++) {
+//			a[i] = i;
+//			sum += a[i];
+//		}
+//		
+//		System.out.println(sum);
+		
+		
+		//문제 14.
+//		int[] num = new int[5];
+//		int Start = 1;
+//		for(int i = 0; i<num.length; i++) {
+//			num[i] = Start;
+//			Start *= 10;
+//			System.out.println(num[i]);
+//		}
+//		
+//		
+//		int[] numb = new int[6];
+//		for(int i =0; i < numb.length; i++) {
+//			numb[i] = 1;
+//			System.out.println(numb[i]);
+//		}
+//		
+//		int[] number = new int[6];
+//		for(int i = 0; i<number.length; i++) {
+//			number[i] = (i+1);
+//			System.out.println(number[i]);
+//		}
+		
+		//문제 15.
+//		int[] numbe = new int[6];
+//		int odd = 1;
+//		
+//		for(int i = 0; i <numbe.length; i++) {
+//			numbe[i] = odd;
+//			odd += 2;
+//			System.out.println(numbe[i]);
+//					
+//		}
+//		
+//		int[] nu = new int[6];
+//		int start = 10;
+//		
+//		for(int i = 0; i<nu.length; i++) {
+//			nu[i] = start;
+//			start += 10;
+//			System.out.println(nu[i]);
+//		}
+		
+//		int[] n = new int[10];
+//		for(int i = 0; i < n.length; i++) {
+//			n[i] = (int)Math.pow(i, 2);
+//			if(i%2==1) {
+//				System.out.println(n[i]);
+//			}
+//		}
+		
+		//문제 16.
+//		int[] user = new int[5];
+//		for(int i = 0; i<user.length; i++) {
+//			user[i] = sc.nextInt();
+//			System.out.printf("user[%d] = %d \n",i,user[i]);
+//		}
+		
+		
+		//문제 17. 
+//		int[] input = new int[10];
+//		int max = input[0];
+//		for(int i = 0; i < input.length; i++) {
+//			input[i] = sc.nextInt();
+//			if(input[i] > max) {
+//				max = input[i];
+//			}
+//			
+//		}
+//			System.out.println(max);
+		
+		// 문제 18.
+//		int[] score = new int[5];
+//		int sum = 0;
+//		double aver = 0;
+//		
+//		for(int i = 0; i < score.length; i++) {
+//			score[i] = sc.nextInt();
+//			sum += score[i];
+//		}
+//		
+//		aver = sum/score.length;
+//		
+//		System.out.println(sum);
+//		System.out.println(aver);
+		
+		
+		//문제 19.
+		
+//		int[] num = {1,2,3,4,5,6,7,8,9,10};
+//		System.out.println("찾을 값을 입력해주세요");
+//		int key = sc.nextInt();
+//		for(int i = 0; i<num.length; i++) {
+//			if(key == num[i]) {
+//				System.out.println("num[" + i +"]");
+//				break;
+//			}else {
+//				System.out.println("없는값입니다. 숫자를 확인해 주세요");
+//				break;
+//			}
+//		}
+		
+		// 문제 20,21,22
+//		int[] a = {10, 50, 39, 25, 90, 11, 100, 44, 38, 99};
+//		int[] b = a;
+//		int key = 50;
+//		String[] c = {"싸이","트와이스","소녀시대","BTS","아이유","브레이브걸스","로제","에이핑크","저스틴비버"}; 
+//		
+//		for(int i = 0; i<b.length; i++) {
+//			if(b[i] == key ) {
+//				System.out.println("b["+i+"]");
+//				System.out.println("c["+i+"] = " + c[i]);
+//			}
+//		
+//		}
+		
+		// 문제 23.
+		
+//		final int Month = 12;
+//		String[] arrString = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+//		String[] arrStringKor = new String[Month];
+//		
+//		
+//		int i;
+//		
+//		System.out.println("영어로");
+//		
+//		for(i = 0; i<arrString.length; i++) {
+//			System.out.println(arrString[i]);
+//		}
+//		
+//		for(i = 0; i<arrStringKor.length; i++) {
+//			arrStringKor[i] = (i+1) + "월";
+//		}
+//		System.out.println("한글로");
+//		
+//		for(i = 0; i <arrStringKor.length; i++) {
+//			System.out.println(arrStringKor[i]);
+//		}
+		
+		
+		
+	}//main
+}// class
